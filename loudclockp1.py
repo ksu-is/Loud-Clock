6762dd0967787e9eb9f6982208f028b945cb7b69
AlexanderWhitman <54565451+AlexanderWhitman@users.noreply.github.com>
2019-12-01 13:48:22 -0500

    Update loudclockp1.py

--- loudclockp1.py
+++ loudclockp1.py
@@ -6,4 +6,11 @@ while True:
     print(str(hours) + ":" + str(minutes) + ":" + str(seconds))
     seconds = seconds + 1
     time.sleep(1)
-    
+    if seconds == 60:
+        seconds = 1
+        minutes = minutes + 1
+    if minutes == 60:
+        hours = hours + 1
+        minutes = 1
+    if hours == 13:
+        hours = 1
